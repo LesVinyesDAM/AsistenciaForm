61df8a841e4ba5618b4bc6ab4a9f2e4d28ce78cc
Alphatule <66392548+alphatule@users.noreply.github.com>
2025-03-26 12:08:13 +0100

    Conexion a bd

--- Asistencia/src/main/java/org/vinyes/asistencia/Database/DatabaseConnection.java
+++ Asistencia/src/main/java/org/vinyes/asistencia/Database/DatabaseConnection.java
@@ -2,15 +2,14 @@ package org.vinyes.asistencia.Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mariadb://localhost/vin_fichaje";
+    private static final String URL = "jdbc:mysql://localhost/vin_fichaje?user=vin&password=vin";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(URL);
     }
 }
