3d69b0a07eba9179890636b101ce44cd8c3a91e9
Thomas <166641342+SHZTD@users.noreply.github.com>
2025-04-07 09:56:52 +0200

    cositas

--- Asistencia/src/main/java/org/vinyes/asistencia/NFCDriver/NFCReader.java
+++ Asistencia/src/main/java/org/vinyes/asistencia/NFCDriver/NFCReader.java
@@ -101,6 +101,11 @@ public class NFCReader {
                         }
                     }
                 }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }).start();
     }
